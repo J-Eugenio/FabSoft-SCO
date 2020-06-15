78ee860572adba73a67843454e4c3bd45270b230
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2020-06-15 10:06:19 -0300

    criação do menu

---
+++ sys/view/header.php
@@ -0,0 +1,8 @@
+<nav class="menu-nav">
+    <ul>
+        <li><a href="../../sys/index.php">Inicio</a></li>
+        <li><a href="../../sys/view/TelaLogin.php">Entrar</a></li>
+        <li><a href="../../sys/view/funcionario/Cadastrar.php">Cadastrar Funcionário</a></li>
+        <li><a href="../../sys//view/usuario/Cadastrar.php">Cadastrar Paciente</a></li>
+    </ul>
+</nav>
